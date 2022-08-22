379020b3f583f779830fd8939bbe893dcec4cef1
Maria Coitinho <106693681+dudabrasilc@users.noreply.github.com>
2022-08-22 01:32:31 -0500

    Add DOMContentLoaded event listener to document Object and GET fetch request on cities data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  fetch("http://localhost:3000/cities")
+  .then(response => response.json())
+  .then(cities => {
+    // console.log(cities)
+    renderCities(cities)
+  })
+})

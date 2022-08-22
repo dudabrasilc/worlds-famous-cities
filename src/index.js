25b51abc3ddffb9e94340dd3f81a9b4acf9739b4
Maria Coitinho <106693681+dudabrasilc@users.noreply.github.com>
2022-08-22 17:20:01 -0500

    Change of button(like button) variable name to "like". Append unlike and cardDiv to the DOM

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     renderCities(cities)
   })
   
+  
   // render cities array from json server 
   function renderCities(cities) {
     cities.forEach(city => {
@@ -14,56 +15,55 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   }
   
-  function renderCity(city) {
+  
+  
+    function renderCity(city) {
       // get node of city cards div
       const citiesCardsDiv = document.getElementById("cities-cards");
       // create a div node of class="card"
       const cardDiv = document.createElement("div")
-    // add class of 'card' to the card div created
+      // add class of 'card' to the card div created
       cardDiv.className = 'card';
       // create children nodes for div with class="card"
       const h2 = document.createElement("h2");
       h2.setAttribute("id", "city-name")
       h2.textContent = city.name;
-    // create img tag
+      // create img tag
       const img = document.createElement("img");
       img.setAttribute("src", city.image);
       img.setAttribute("class", "city-img")
-    // create p tag that is going to have the like count
+      // create p tag that is going to have the like count
       const p = document.createElement("p");
       p.className = "likes-count";
       p.textContent = `${city.likes} likes`;
-    // create like button
-      const button = document.createElement("button");
-      button.setAttribute("class", "like-btn");
-    button.setAttribute("id", city.id);
-      button.textContent = "👍";
-    // button event listener
-      button.addEventListener('click', () => {
+      // create like button
+      const like = document.createElement("button");
+      like.setAttribute("class", "like-btn");
+      like.setAttribute("id", city.id);
+      like.textContent = "👍";
+      // button event listener
+      like.addEventListener('click', () => {
         let count = 0;
         count = city.likes++ +1
         // console.log(count);
         p.textContent = `${count} likes`;
         updateLikes(city, count);
       })
-    const unlike = document.createElement("button");
+      const unlike = document.createElement("button");
       unlike.setAttribute("class", "unlike-btn");
-    unlike.setAttribute("id", city.id);
+      unlike.setAttribute("id", city.id);
       unlike.textContent = "👎";
-    unlike.addEventListener("click", () => {
+      unlike.addEventListener("click", () => {
         let count = city.likes;
         count = city.likes-- - 1;
         p.textContent = `${count} likes`;
         updateLikes(city, count)
       })
-    // append created elements to the DOM
+      // append created elements to the DOM
       cardDiv.appendChild(h2)
       cardDiv.appendChild(img)
-    cardDiv.appendChild(p)
-      cardDiv.appendChild(button)
-    
-  }
-  
-})
-
-
+      cardDiv.appendChild(p)
+      cardDiv.appendChild(like)
+      cardDiv.appendChild(unlike)
+      citiesCardsDiv.appendChild(cardDiv)
+      }

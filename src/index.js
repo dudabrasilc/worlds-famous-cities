16a3eff317c831f86a9fbc96afa1727880e7d80b
Maria Coitinho <brasildu@gmail.com>
2022-11-08 16:51:10 -0600

    General organization and identation of js file

--- src/index.js
+++ src/index.js
@@ -3,84 +3,71 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("http://localhost:3000/cities")
   .then(response => response.json())
   .then(cities => {
-    // console.log(cities)
     renderCities(cities)
   })
   
   
-  // render cities array from json server 
   function renderCities(cities) {
     cities.forEach(city => {
       renderCity(city)
     })
   }
   
-  
-  
-    function renderCity(city) {
-      // get node of city cards div
-      const citiesCardsDiv = document.getElementById("cities-cards");
-      // create a div node of class="card"
-      const cardDiv = document.createElement("div")
-      // add class of 'card' to the card div created
-      cardDiv.className = 'card';
-      // create children nodes for div with class="card"
-      const h2 = document.createElement("h2");
-      h2.setAttribute("id", "city-name")
-      h2.textContent = city.name;
-      // create img tag
-      const img = document.createElement("img");
-      img.setAttribute("src", city.image);
-      img.setAttribute("class", "city-img")
-      // create p tag that is going to have the like count
-      const p = document.createElement("p");
-      p.className = "likes-count";
-      p.textContent = `${city.likes} likes`;
-      // create like button
-      const like = document.createElement("button");
-      like.setAttribute("class", "like-btn");
-      like.setAttribute("id", city.id);
-      like.textContent = "👍";
-      // button event listener
-      like.addEventListener('click', () => {
-        let count = 0;
-        count = city.likes++ +1
-        // console.log(count);
-        p.textContent = `${count} likes`;
-        updateLikes(city, count);
-      })
-      const unlike = document.createElement("button");
-      unlike.setAttribute("class", "unlike-btn");
-      unlike.setAttribute("id", city.id);
-      unlike.textContent = "👎";
-      unlike.addEventListener("click", () => {
-        let count = city.likes;
-        count = city.likes-- - 1;
-        p.textContent = `${count} likes`;
-        updateLikes(city, count)
-      })
-      // append created elements to the DOM
-      cardDiv.appendChild(h2)
-      cardDiv.appendChild(img)
-      cardDiv.appendChild(p)
-      cardDiv.appendChild(like)
-      cardDiv.appendChild(unlike)
-      citiesCardsDiv.appendChild(cardDiv)
-      }
-  
-  
-      function updateLikes(city, count) {
-        // console.log(e)
-      fetch(`http://localhost:3000/cities/${city.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "likes": count
-      })
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))  
-    }
+
+  function renderCity(city) {
+    const citiesCardsDiv = document.getElementById("cities-cards");
+    const cardDiv = document.createElement("div")
+    cardDiv.className = 'card';
+    const h2 = document.createElement("h2");
+    h2.setAttribute("id", "city-name")
+    h2.textContent = city.name;
+
+    const img = document.createElement("img");
+    img.setAttribute("src", city.image);
+    img.setAttribute("class", "city-img")
+    const p = document.createElement("p");
+    p.className = "likes-count";
+    p.textContent = `${city.likes} likes`;
+    const like = document.createElement("button");
+    like.setAttribute("class", "like-btn");
+    like.setAttribute("id", city.id);
+    like.textContent = "👍";
+    like.addEventListener('click', () => {
+      let count = 0;
+      count = city.likes++ +1
+      p.textContent = `${count} likes`;
+      updateLikes(city, count);
+    })
+    const unlike = document.createElement("button");
+    unlike.setAttribute("class", "unlike-btn");
+    unlike.setAttribute("id", city.id);
+    unlike.textContent = "👎";
+    unlike.addEventListener("click", () => {
+      let count = city.likes;
+      count = city.likes-- - 1;
+      p.textContent = `${count} likes`;
+      updateLikes(city, count)
+    })
+    cardDiv.appendChild(h2)
+    cardDiv.appendChild(img)
+    cardDiv.appendChild(p)
+    cardDiv.appendChild(like)
+    cardDiv.appendChild(unlike)
+    citiesCardsDiv.appendChild(cardDiv)
+  }
+
+
+  function updateLikes(city, count) {
+    fetch(`http://localhost:3000/cities/${city.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      "likes": count
+    })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))  
+  }
 })
